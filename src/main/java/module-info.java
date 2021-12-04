3bfcaee1c857c7eec01d4a17af07bda22a7215db
Michael Reid <slytherinrulesall@gmail.com>
2021-12-04 17:31:27 +0000

    created test methods for each check in the email and password fields

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,6 @@ module com.example.gooey {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires com.dlsc.formsfx;
-
     opens com.example.gooey to javafx.fxml;
     exports com.example.gooey;
 }
